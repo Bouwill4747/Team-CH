09cafdf0d7e7c467d1cd5d4f9e5b150ab885f19a
Will <Williambourbonniere110@gmail.com>
2025-11-07 14:10:42 -0500

    Premier commit Will pour Classe Adherant, Emprunt, 3 Menu test (On va decidé plus tard lequel) et aussi la liste de livre en format CSV

---
+++ Classe_Adherent.py
@@ -0,0 +1,13 @@
+class Adherent:
+
+    def __init__(self, nom: str, prenom: str, id: int):
+        self.nom = nom
+        self.prenom = prenom
+        self.id = id
+
+    def emprunterLivre(self):
+        pass
+
+    def rendreLivre(self):
+        pass
+
