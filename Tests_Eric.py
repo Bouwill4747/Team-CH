15c4a456e2624316b459c2c7dd90d07ed1006c19
edc-null <149556401+edc-null@users.noreply.github.com>
2025-12-11 17:16:01 -0500

    arranger import/sauvegarde

--- Tests_Eric.py
+++
@@ -1,8 +0,0 @@
-from Adherent import Adherent
-from Document import Livre
-from Emprunt import Emprunt
-from Bibliotheque import Bibliotheque
-
-biblio = Bibliotheque("Bibliothèque BdeB")
-# biblio.liste_adherents[0].emprunter_livre(biblio)
-Adherent.rendre_livre(biblio)
--- Tests_Valerie.py
+++
@@ -1,9 +0,0 @@
-from Adherent import Adherent
-from Document import Livre
-from Emprunt import Emprunt
-from Bibliotheque import Bibliotheque
-
-biblio = Bibliotheque("Bibliothèque BdeB")
-# biblio.liste_adherents[0].emprunter_livre(biblio)
-for x in biblio.liste_emprunts:
-    print(x)
